a3c8a54c63ccb93b725c8481886e748fd048dc78
nandobfer <nandobfer@yahoo.com.br>
2025-01-22 12:30:36 -0300

    feat: switched deploy to yarn script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-yarn build
-zip -r ecl.zip dist
-scp ecl.zip boz:/var/www/ 
-
-ssh boz "cd /var/www && unzip ecl.zip && rm -rf ecl.nandoburgos.dev.bak/ && mv ecl.nandoburgos.dev/ ecl.nandoburgos.dev.bak && mv dist/ ecl.nandoburgos.dev && rm -rf ecl.zip"
