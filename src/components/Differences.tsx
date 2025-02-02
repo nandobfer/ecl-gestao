3aaf0dfdce09ce1e0d62295351b132de97d4ec1f
nandobfer <nandobfer@yahoo.com.br>
2025-02-02 19:51:13 -0300

    fix: overflowing

--- src/components/Differences.tsx
+++ src/components/Differences.tsx
@@ -19,7 +19,6 @@ export const Differences: React.FC<DifferencesProps> = ({}) => {
                 flexDirection: "column",
                 gap: "5vw",
                 position: "relative",
-                overflowX: "hidden",
             }}
         >
             <Box sx={{ gap: isMobile ? "10vw" : "3vw", alignItems: "flex-end", flexDirection: isMobile ? "column" : "row" }}>
